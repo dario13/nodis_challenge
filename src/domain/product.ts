52f5d32f6b6989ed1a3d2da0790f1c44fbc86d21
Dario G <dariogabalec@gmail.com>
2021-01-13 09:53:33 -0300

    feat: :sparkles: Classes added for product, user_product and user

---
+++ src/domain/product.ts
@@ -0,0 +1,51 @@
+import { Gtin13 } from "./value_objects/gtin13";
+import { Uuid } from "./value_objects/uuid";
+
+export class Product {
+  private _id: Uuid;
+  private _name: string;
+  private _description: string;
+  private _gtin13: Gtin13;
+  private _images: Array<string>;
+
+  constructor(
+    name: string,
+    description: string,
+    gtin13: string,
+    images: Array<string>
+  ) {
+    this._id = Uuid.random();
+    this._name = name;
+    this._description = description;
+    this._gtin13 = new Gtin13(gtin13);
+    this._images = images;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get images() {
+    return this._images;
+  }
+
+  set name(newName: string) {
+    this._name = newName;
+  }
+
+  set description(newDescription) {
+    this._description = newDescription;
+  }
+
+  set images(newImages: Array<string>) {
+    this._images = newImages;
+  }
+}
---
+++ src/domain/user_product.ts
@@ -0,0 +1,86 @@
+import { InvalidArgument } from "./exceptions/invalid_argument";
+import { Product } from "./product";
+import { User } from "./user";
+
+enum Status {
+  created,
+  updated,
+  deleted,
+}
+
+export class UserProduct {
+  private _user: User;
+  private _product: Product;
+  private _price: number;
+  private _quantity: number;
+  private _status: Status;
+  private _createdAt: Date;
+  private _updatedAt: Date;
+  private _deletedAt: Date;
+
+  constructor(user: User, product: Product, price: number, quantity: number) {
+    this.ensurePriceIsBiggerThanZero(price);
+    this.ensureIsValidQuantityForCreation(quantity);
+    this._user = user;
+    this._product = product;
+    this._price = price;
+    this._quantity = quantity;
+    this._status = Status.created;
+    this._createdAt = new Date(Date.now());
+  }
+
+  private ensureIsValidQuantityForCreation(quantity: number) {
+    if (quantity <= 0) throw new InvalidArgument("quantity");
+  }
+
+  private ensureIsValidQuantityForUpdate(quantity: number) {
+    if (quantity < 0) throw new InvalidArgument("quantity");
+  }
+
+  private ensureIsNotDeleted() {
+    if (this._status === Status.deleted) {
+      throw new Error("Cant update deleted product");
+    }
+  }
+
+  private ensurePriceReductionIsNotLessThanFiftyPercent(newPrice: number) {
+    if (newPrice <= this._price * 0.5)
+      throw new Error("The new price cannot have a discount of more than 50%");
+  }
+
+  private ensurePriceIsBiggerThanZero(newPrice: number) {
+    if (newPrice <= 0)
+      throw new Error("The price cannot be less than or equal to zero");
+  }
+
+  set price(newPrice: number) {
+    this.ensureIsNotDeleted();
+    this.ensurePriceIsBiggerThanZero(newPrice);
+    this.ensurePriceReductionIsNotLessThanFiftyPercent(newPrice);
+    this._price = newPrice;
+    this._status = Status.updated;
+    this._updatedAt = new Date(Date.now());
+  }
+
+  set quantity(newQuantity: number) {
+    this.ensureIsNotDeleted();
+    this.ensureIsValidQuantityForUpdate(newQuantity);
+    this._quantity = newQuantity;
+    this._status = Status.updated;
+    this._updatedAt = new Date(Date.now());
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  get quantity() {
+    return this._quantity;
+  }
+
+  public delete() {
+    this.ensureIsNotDeleted();
+    this._status = Status.deleted;
+    this._deletedAt = new Date(Date.now());
+  }
+}
